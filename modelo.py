b8fd33d3a36cfffa9ab5a61578e39c28342117d0
Gallowzz <134352607+Gallowzz@users.noreply.github.com>
2025-09-12 22:07:27 -0600

    Parte 3 Terminada
    Archivo llamado modelo,py

--- modelo.py
+++ modelo.py
@@ -5,6 +5,8 @@ import json
 
 # Cargar Data de Entrenamiento
 data = np.load("mnist_train.npz")
+input = data["images"].reshape(-1, 784) / 255
+targets = data["labels"]
 
 # Abrir Archivo JSON
 with open("mnist_mlp_pretty.json","r") as ah:
@@ -14,18 +16,19 @@ with open("mnist_mlp_pretty.json","r") as ah:
 layer0 = DnnLib.DenseLayer(784, 128, DnnLib.ActivationType.RELU)
 layer1 = DnnLib.DenseLayer(128, 10, DnnLib.ActivationType.SOFTMAX)
 
-# Datos Capa 0:
-layer0.weights = np.array(datos["layers"][0]["W"])
-layer0.bias = np.array(datos["layers"][0]["b"])
+# Datos Capa 0
+layer0.weights = np.array(datos["layers"][0]["W"]).T
+layer0.bias = np.array(datos["layers"][0]["b"]).T
 
-output0 = layer0.forward(data["images"])
+# Datos Capa 1
+layer1.weights = np.array(datos["layers"][1]["W"]).T
+layer1.bias = np.array(datos["layers"][1]["b"]).T
 
-# Datos Capa 1:
-layer1.weights = np.array(datos["layers"][1]["W"])
-layer1.bias = np.array(datos["layers"][1]["b"])
-
+# Forward Pass
+output0 = layer0.forward(input)
 output1 = layer1.forward(output0)
 
-# Salidas
-print("Salida Capa 0: "+output0)
-print("Salida Capa 1: "+output1)
+# Precision
+predictions = np.argmax(output1, axis=1)
+accuracy = np.mean(predictions == targets)
+print("Precision: ",accuracy)
